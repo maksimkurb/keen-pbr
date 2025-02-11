ed6adda6420785b1fefee58bb8c798620cbcd012
Maxim Kurbatov <max@cubly.ru>
2025-02-12 00:01:40 +0500

    Update for new dnsmasq config generation method

--- keenetic-pbr/opt/etc/ndm/ifstatechanged.d/50-keenetic-pbr-routing.sh
+++ keenetic-pbr/opt/etc/ndm/ifstatechanged.d/50-keenetic-pbr-routing.sh
@@ -1,9 +1,9 @@
 #!/opt/bin/sh
 
-. /opt/etc/keenetic-pbr/defaults
-
 # Check if invoked with "hook"
 [ "$1" = "hook" ] || exit 0
+
+. /opt/etc/keenetic-pbr/defaults
 
 $KEENETIC_PBR -config "$CONFIG" apply -only-routing-for-interface "$system_name" -fail-if-nothing-to-apply
 status=$?
@@ -11,7 +11,12 @@ status=$?
 if [ "$status" = "0" ]; then
   logger -t "keenetic-pbr" "Routing applied for interface $system_name (ifstatechanged.d hook)"
 
-  # Do your own stuff here
+  # If you want to add some additional commands after routing is applied, you can add them
+  # to the /opt/etc/keenetic-pbr/hook.sh script
+  if [ -f "/opt/etc/keenetic-pbr/hook.sh" ]; then
+    keenetic_pbr_hook="ifstatechanged"
+    . /opt/etc/keenetic-pbr/hook.sh
+  fi
 fi
 
 exit 0
--- keenetic-pbr/opt/etc/ndm/netfilter.d/50-keenetic-pbr-fwmarks.sh
+++ keenetic-pbr/opt/etc/ndm/netfilter.d/50-keenetic-pbr-fwmarks.sh
@@ -3,10 +3,17 @@
 #[ "$type" == "ip6tables" ] && exit 0
 [ "$table" != "mangle" ] && exit 0
 
-/opt/etc/init.d/S80keenetic-pbr apply-routing
+. /opt/etc/keenetic-pbr/defaults
+
+$KEENETIC_PBR -config "$CONFIG" apply -skip-ipset
 
 logger -t "keenetic-pbr" "Routing applied for all interfaces (netfilter.d hook)"
 
-# Do your own stuff here
+# If you want to add some additional commands after routing is applied, you can add them
+# to the /opt/etc/keenetic-pbr/hook.sh script
+if [ -f "/opt/etc/keenetic-pbr/hook.sh" ]; then
+  keenetic_pbr_hook="netfilter"
+  . /opt/etc/keenetic-pbr/hook.sh
+fi
 
 exit 0
