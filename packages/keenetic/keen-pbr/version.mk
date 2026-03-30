996287a1d028c2a2170096ae84d7ba0b3fa04a77
Maxim Kurbatov <maksimkurb@gmail.com>
2026-03-30 10:52:33 +0500

    packaging: require project version file without fallback

--- packages/keenetic/keen-pbr/version.mk
+++ packages/keenetic/keen-pbr/version.mk
@@ -2,9 +2,20 @@ version_mk_dir := $(dir $(lastword $(MAKEFILE_LIST)))
 keen_pbr_repo_version_mk := $(abspath $(version_mk_dir)../../../version.mk)
 keen_pbr_has_project_version := $(shell [ -f "$(keen_pbr_repo_version_mk)" ] && grep -q '^KEEN_PBR_VERSION[[:space:]]*=' "$(keen_pbr_repo_version_mk)" && echo yes)
 
-ifeq ($(keen_pbr_has_project_version),yes)
-include $(keen_pbr_repo_version_mk)
+ifeq ($(wildcard $(keen_pbr_repo_version_mk)),)
+$(error Missing KEEN_PBR project version file: $(keen_pbr_repo_version_mk))
 endif
 
-KEEN_PBR_VERSION ?= 3.0.0
-KEEN_PBR_RELEASE ?= 1
+ifneq ($(keen_pbr_has_project_version),yes)
+$(error KEEN_PBR_VERSION is not defined in $(keen_pbr_repo_version_mk))
+endif
+
+include $(keen_pbr_repo_version_mk)
+
+ifeq ($(strip $(KEEN_PBR_VERSION)),)
+$(error KEEN_PBR_VERSION is empty in $(keen_pbr_repo_version_mk))
+endif
+
+ifeq ($(strip $(KEEN_PBR_RELEASE)),)
+$(error KEEN_PBR_RELEASE is empty in $(keen_pbr_repo_version_mk))
+endif
