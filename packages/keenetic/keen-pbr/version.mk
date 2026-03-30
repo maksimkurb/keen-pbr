8d497fd255e992c58a6b7c6613719a89df27cf13
Maxim Kurbatov <maksimkurb@gmail.com>
2026-03-30 11:32:08 +0500

    packaging: load version from KEEN_PBR_SRC to avoid SDK version.mk

--- packages/keenetic/keen-pbr/version.mk
+++ packages/keenetic/keen-pbr/version.mk
@@ -1,13 +1,7 @@
-version_mk_dir := $(dir $(lastword $(MAKEFILE_LIST)))
-keen_pbr_repo_version_mk := $(abspath $(version_mk_dir)../../../version.mk)
-keen_pbr_has_project_version := $(shell [ -f "$(keen_pbr_repo_version_mk)" ] && grep -q '^KEEN_PBR_VERSION[[:space:]]*=' "$(keen_pbr_repo_version_mk)" && echo yes)
+keen_pbr_repo_version_mk := $(abspath $(KEEN_PBR_SRC)/version.mk)
 
 ifeq ($(wildcard $(keen_pbr_repo_version_mk)),)
 $(error Missing KEEN_PBR project version file: $(keen_pbr_repo_version_mk))
-endif
-
-ifneq ($(keen_pbr_has_project_version),yes)
-$(error KEEN_PBR_VERSION is not defined in $(keen_pbr_repo_version_mk))
 endif
 
 include $(keen_pbr_repo_version_mk)
