601586466587c3445e55b3662a1ea5e8c01f76b6
Maxim Kurbatov <maksimkurb@gmail.com>
2026-03-30 11:53:19 +0500

    packaging: make package version files self-contained constants

--- packages/openwrt/keen-pbr/version.mk
+++ packages/openwrt/keen-pbr/version.mk
@@ -1,15 +1,2 @@
-keen_pbr_repo_version_mk := $(abspath $(KEEN_PBR_SRC)/version.mk)
-
-ifeq ($(wildcard $(keen_pbr_repo_version_mk)),)
-$(error Missing KEEN_PBR project version file: $(keen_pbr_repo_version_mk))
-endif
-
-include $(keen_pbr_repo_version_mk)
-
-ifeq ($(strip $(KEEN_PBR_VERSION)),)
-$(error KEEN_PBR_VERSION is empty in $(keen_pbr_repo_version_mk))
-endif
-
-ifeq ($(strip $(KEEN_PBR_RELEASE)),)
-$(error KEEN_PBR_RELEASE is empty in $(keen_pbr_repo_version_mk))
-endif
+KEEN_PBR_VERSION:=3.0.0
+KEEN_PBR_RELEASE:=1
